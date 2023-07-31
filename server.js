671d75c083018cb877e1cff165cb27acc9f87490
adarshsofueled <adarsh.raj@sofueled.com>
2023-07-31 17:07:14 +0530

    second commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const { resolvers } = require("./src/resolvers/index");
 const Schema = require("./src/schema/index");
 const { createHandler } = require("graphql-http/lib/use/express");
-
+const cors=require("cors")
 
 const app = express();
-
+app.use(cirs())
 app.use(
   "/graphql",
   
